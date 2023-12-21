184e8ee8f947faaf63b780d1350c78d04f691239
sandypratamaa <102770747+sandypratamaa@users.noreply.github.com>
2023-12-21 12:12:38 +0700

    Update streamlt.py

--- streamlt.py
+++ streamlt.py
@@ -3,7 +3,7 @@ import streamlit as st
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 # load save model
-model_fraud = pickle.load(open('model_spam_tfidf_nb.model','rb'))
+model_fraud = pickle.load(open('model_spam_tfidf_nb.sav','rb'))
 
 tfidf = TfidfVectorizer
 
